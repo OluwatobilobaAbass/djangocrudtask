be736a54cdcbe8cce5470e0a34ca0e72578c0ce5
OluwatobilobaAbass <105176168+OluwatobilobaAbass@users.noreply.github.com>
2022-06-27 23:58:24 +0100

    work done

--- I4G003343W98/blog/admin.py
+++ I4G003343W98/blog/admin.py
@@ -3,8 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
-<<<<<<< HEAD
 admin.site.register(Post)
-=======
-admin.site.register(Post)
->>>>>>> a7319c70caa8f78d046d12419014e5225e76aa05
--- I4G003343W98/blog/views.py
+++ I4G003343W98/blog/views.py
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-=======
-from django.shortcuts import render
-from django.urls import reverse_lazy
->>>>>>> a7319c70caa8f78d046d12419014e5225e76aa05
 from django.views import generic
 from .models import Post
 from django.urls import reverse_lazy
-
-<<<<<<< HEAD
 
 # Create your views here.
 class PostDetailView(generic.DetailView):
@@ -31,34 +24,9 @@ class PostUpdateView(generic.UpdateView):
     fields = "__all__"
     success_url  = reverse_lazy('blog:all')
     template_name = 'post_form.html'
+
 class PostDeleteView(generic.DeleteView):
     model = Post
     fields = "__all__"
     success_url  = reverse_lazy('blog:all')
     template_name = 'post_confirm_delete.html'
-=======
-class PostListView(generic.ListView):
-    model = Post
-    template_name = "templates/blog/post_list.html"
-
-class PostCreateView(generic.CreateView):
-    model = Post
-    fields = "__all__"
-    success_url  = reverse_lazy("blog:all")
-    template_name = "templates/blog/post_form.html"
-
-class PostDetailView(generic.DetailView):
-    model = Post
-    template_name = "templates/blog/post_detail.html"
-class PostUpdateView(generic.UpdateView):
-    model = Post
-    fields = "__all__"
-    success_url  = reverse_lazy("blog:all")
-    template_name = "templates/blog/post_form.html"
-
-class PostDeleteView(generic.DeleteView):
-    model = Post
-    fields = "__all__"
-    success_url  = reverse_lazy("blog:all")
-    template_name = "templates/blog/post_delete.html"
->>>>>>> a7319c70caa8f78d046d12419014e5225e76aa05
